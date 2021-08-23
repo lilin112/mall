54c752a2c220dc2d44a7c20673ee409bff5658c2
李琳 <2108472412@qq.com>
2021-08-23 17:44:51 +0800

    第二次提交

---
+++ src/main.js
@@ -0,0 +1,9 @@
+/* eslint-disable eol-last */
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'normalize.css'
+import './style/index.scss'
+
+createApp(App).use(store).use(router).mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,57 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-undef */
+/* eslint-disable indent */
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/home/Home.vue'
+import Login from '../views/login/Login'
+import Register from '../views/register/Register.vue'
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+        // eslint-disable-next-line space-before-function-paren
+        beforeEnter(to, from, next) {
+            const { isLogin } = localStorage
+            isLogin ? next({ name: 'Home' }) : next()
+        }
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+        beforeEnter(to, from, next) {
+            const { isLogin } = localStorage
+            isLogin ? next({ name: 'Home' }) : next()
+        }
+    }
+    // {
+    //   path: '/about',
+    //   name: 'About',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    const { isLogin } = localStorage
+    const { name } = to
+    const isLoginOrRegister = (name === 'Login' || name === 'Register');
+    // eslint-disable-next-line space-infix-ops
+    (isLogin || isLoginOrRegister) ? next(): next({ name: 'Login' })
+})
+
+// eslint-disable-next-line eol-last
+export default router
